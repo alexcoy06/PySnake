bd0be6e6e3d915b27c0b64f240654dd513db1453
Alexander Coy <alexcoy06@gmail.com>
2025-03-20 19:52:40 -0500

    Event Listening Completed

--- snake.py
+++ snake.py
@@ -3,21 +3,23 @@ from turtle import Turtle
 # Constants for key values
 DISTANCE = 20
 BODY_LENGTH = 3
-POSITION = 0
 
 class Snake:
     
     def __init__(self):
         self.snake_body = []
         self.create_snake()
+        self.head = self.snake_body[0]
     
     # Creates the body with length across the x    
     def create_snake(self):
+        position = 0
+        
         for segment in range(BODY_LENGTH):
             new_segment = Turtle('square')
             new_segment.color('white')
             new_segment.penup()
-            new_segment.goto(POSITION, 0)
+            new_segment.goto(position, 0)
             self.snake_body.append(new_segment)
             position -= 20
     
@@ -27,4 +29,20 @@ class Snake:
             new_x = self.snake_body[segment - 1].xcor()
             new_y = self.snake_body[segment - 1].ycor()
             self.snake_body[segment].goto(new_x, new_y)
-        self.snake_body[0].forward(DISTANCE)
+        self.head.forward(DISTANCE)
+    
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+    
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+    
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+    
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
--- snake_game.py
+++ snake_game.py
@@ -11,9 +11,21 @@ screen.tracer(0)
 
 snake = Snake()
 
+# Event listening for button presses   
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.up, 'w')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.down, 's')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.left, 'a')
+screen.onkey(snake.right, 'Right')
+screen.onkey(snake.right, 'd')
+
 # The functioning game
 game_on = True
 while game_on:
+    
     #smoothing movement transition
     screen.update()
     time.sleep(0.1)
